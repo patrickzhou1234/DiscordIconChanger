4146f5e920a47eec5a2cfa9bf87da042b0649ff5
Patrick Zhou <patrickchang44543@gmail.com>
2023-11-21 17:59:52 -0800

    Create GalacticChrome.js

---
+++ GalacticChrome.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         DiscordIconChanger
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=discord.com
+// @grant        none
+// ==/UserScript==
+
+setTimeout(() => {
+var injectHTML = '<img src="/assets/a28b009dcc138c849209.webp" width="48" alt="App Icon" draggable="false"></div>';
+document.querySelectorAll("foreignobject")[0].childNodes[0].childNodes[0].innerHTML=injectHTML;
+}, 5000);
